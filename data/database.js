371776db2be539db9e6d771d4b39c21d7cd37c37
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2022-11-21 00:46:37 +0300

    fixed the database connect error

--- data/database.js
+++ data/database.js
@@ -7,7 +7,7 @@ let database;
 
 
 async function connectToDatabase(){
-    const client = await MongoClient.connect('https://localhost:27017')
+    const client = await MongoClient.connect('mongodb://localhost:27017')
     database = client.db('online-shop');
 }
 
