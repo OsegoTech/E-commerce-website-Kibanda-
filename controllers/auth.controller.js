6cc54e8535dd90df4378cffc21bd872f155069ee
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2022-11-21 00:31:17 +0300

    Defined folder structure, added basic view pages, implemented the MVC software architecture pattern

---
+++ controllers/auth.controller.js
@@ -0,0 +1,17 @@
+const getSignup = (req, res) => {
+    res.render('customer/auth/signup');
+};
+
+const signup = (req, res) => {
+    
+}
+
+const getLogin = (req, res) => {
+
+};
+
+module.exports = {
+    getSignup: getSignup,
+    getLogin: getLogin,
+    signup: signup
+};
